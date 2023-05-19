cfb1b66baafddf5ba1fb7962b93b254c6c79d0a2
drew <58788781+drew138@users.noreply.github.com>
2023-05-19 11:25:13 -0500

    fix

--- orchestrator/src/server/server.py
+++ orchestrator/src/server/server.py
@@ -11,9 +11,11 @@ class Server:
         self.port = port
 
     def start(self) -> None:
+        print("Starting server controller grpc", flush=True)
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         register_pb2_grpc.add_RegisterServiceServicer_to_server(
             self.service, server)
-        server.add_insecure_port(f'[::]:{self.port}')
+        server.add_insecure_port(f'0.0.0.0:{self.port}')
         server.start()
+        print("GRPC ready", flush=True)
         server.wait_for_termination()
