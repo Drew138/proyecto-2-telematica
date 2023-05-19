28b1ed42d8baf1695266fec0e8c16aca1d02cd31
drew <58788781+drew138@users.noreply.github.com>
2023-05-18 20:53:43 -0500

    fix

--- orchestrator/src/monitor/monitor.py
+++ orchestrator/src/monitor/monitor.py
@@ -3,7 +3,7 @@ from client.client import Client
 
 
 class Monitor:
-    def __init__(self, instance):
+    def __init__(self, instance) -> None:
         self.instance = instance
         self.client: Client = self._create_client()
 
@@ -13,28 +13,26 @@ class Monitor:
     def get_instance(self):
         return self.instance
 
-    def _create_client(self):
+    def _create_client(self) -> Client:
         return Client(self.instance.get_socket())
 
-    @staticmethod
-    def safe_grpc_call(function):
-        def inner(self, *args, **kwargs):
-            for _ in range(5):
-                try:
-                    return_value = function(self, *args, **kwargs)
-                    return return_value
-                except Exception:
-                    pass
-            instance = self.get_instance()
-            return instance.remove_instance(instance.id)
-        return inner
+    # @staticmethod
+    # def safe_grpc_call(function):
+    #     def inner(self, *args, **kwargs):
+    #         for _ in range(5):
+    #             try:
+    #                 return_value = function(self, *args, **kwargs)
+    #                 return return_value
+    #             except Exception:
+    #                 pass
+    #         instance = self.get_instance()
+    #         return instance.remove_instance(instance.id)
+    #     return inner
 
-    @safe_grpc_call
-    def ping(self):
+    def ping(self) -> None:
         self.client.monitor_stub.Ping()
 
-    @safe_grpc_call
-    def update_metric(self):
+    def update_metric(self) -> None:
         metric_response: MetricResponse = self.client.monitor_stub.GetMetrics()
         self.metric: int = metric_response.message
 
