47f0263dba03c04a5101656a62c8be4bf6d942cb
drew <58788781+drew138@users.noreply.github.com>
2023-05-19 14:32:50 -0500

    fix

--- orchestrator/src/main.py
+++ orchestrator/src/main.py
@@ -34,7 +34,8 @@ def main() -> None:
     threading.Thread(target=server.start).start()
 
     for _ in range(config['policy_config']['min_instances']):
-        threading.Thread(target=Instance, args=[config]).start()
+        t = threading.Thread(target=Instance, args=[config]).start()
+        t.join()
 
     #inst = Instance(config)
 
